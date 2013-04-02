84652510c41be9be7a03a5a740e6f03f99cc3a77
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-04-01 17:20:39 -0700

    add .ready()

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,7 @@
-// put your JavaScript and jQuery code here ...
+$(document).ready(function() {
+  // This is called after the document has loaded in its entirety
+  // This guarantees that any elements we bind to will exist on the page
+  // when we try to bind to them
+
+  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+});

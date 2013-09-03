93e2f7e56574a32de6714b6fc1714b1bd4d953ee
Ed Shadi <edshadi@gmail.com>
2013-09-03 10:34:00 -0700

    move the sinatra app configuration to environment.rb and explicitly set the app root.

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,18 @@ APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
 
+configure do
+  # By default, Sinatra assumes that the root is the file that calls the configure block.
+  # Since this is not the case for us, we set it manually.
+  set :root, APP_ROOT.to_path
+  # See: http://www.sinatrarb.com/faq.html#sessions
+  enable :sessions
+  set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
+
+  # Set the views to
+  set :views, File.join(Sinatra::Application.root, "app", "views")
+end
+
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }

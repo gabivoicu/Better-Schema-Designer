6023bf3e5ee3a5c28be7e1765b0ca695ae75456f
Ed Shadi <edshadi@gmail.com>
2013-09-03 10:34:49 -0700

    require shoulda-matchers and racker/test, include rack test methods and set the app in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,13 @@ require 'rubygems'
 ENV['RACK_ENV'] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
+require 'shoulda-matchers'
+require 'rack/test'
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
+
+def app
+  Sinatra::Application
+end
